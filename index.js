6913fc168dc0efaaa3fcf4ab519dd676743c26d6
Akshai Dev <akshaimuraleedharan4@gmail.com>
2024-06-05 16:39:40 +0100

    nav update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const ulList = document.querySelector('#list_con');
 const toggleBtn =document.querySelector('#span_toggle')
-
+const blurScreen =document.querySelector('#blur_screen')
 function toggleButton() {
+
    ulList.classList.add('trans')
+   blurScreen.classList.add('blur_trans')
 }
 
 toggleBtn.addEventListener('click',toggleButton)
