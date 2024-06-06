f0f577ecffd5e15eab7911c112e2f888c126c1b4
Akshai Dev <akshaimuraleedharan4@gmail.com>
2024-06-06 15:27:02 +0100

    responsive update

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const ulList = document.querySelector('#list_con');
-const toggleBtn =document.querySelector('#span_toggle')
-const blurScreen =document.querySelector('#blur_screen')
+const toggleBtn =document.querySelector('#span_toggle');
+const blurScreen =document.querySelector('#blur_screen');
+ const heroContent =document.querySelector('#hero_content');
 
+console.log(heroContent);
 
 // toggle boutton for responsive
 
@@ -10,7 +12,7 @@ function toggleButton() {
     ulList.classList.toggle('trans')
     blurScreen.classList.toggle('blur_trans')
     toggleBtn.classList.toggle('close')
-
+     heroContent.classList.toggle('hero_content')
 }
 
 toggleBtn.addEventListener('click',toggleButton)
